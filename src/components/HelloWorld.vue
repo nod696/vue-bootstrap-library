58e1ac0d44e317b0cd739d63c3a6a9ae770a1da0
nod696 <tremor696@rambler.ru>
2023-02-20 18:59:16 +0500

    VueBootstrap library

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,44 +1,118 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <MultiRangeSlider
+      :min="1970"
+      :max="2023"
+      :step="1"
+      :ruler="true"
+      :label="true"
+      :minValue="barMinValue"
+      :maxValue="barMaxValue"
+      @input="UpdateValues"
+    />
+    <b-col lg="6" class="my-1">
+      <b-form-group
+        label="Filter"
+        label-for="filter-input"
+        label-cols-sm="3"
+        label-align-sm="right"
+        label-size="sm"
+        class="mb-0"
+      >
+        <b-input-group size="sm">
+          <b-form-input
+            id="filter-input"
+            v-model="filter"
+            type="search"
+            placeholder="Type to Search"
+          ></b-form-input>
+
+          <b-input-group-append>
+            <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
+          </b-input-group-append>
+        </b-input-group>
+      </b-form-group>
+    </b-col>
+    <b-table
+      :no-local-sorting="true"
+      striped
+      hover
+      :small="true"
+      :items="filteredData"
+      :fields="fields"
+      :filter="filter"
+      @sort-changed="sortChanged"
+      :filter-included-fields="filterOn"
+    ></b-table>
   </div>
 </template>
 
 <script>
+import MultiRangeSlider from "multi-range-slider-vue";
+import data from "./../../data.json";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  components: {
+    MultiRangeSlider,
+  },
+  data() {
+    return {
+      barMinValue: 1970,
+      barMaxValue: 2023,
+      parsedData: data,
+      fields: [
+        {
+          key: "id",
+          sortable: true,
+        },
+        {
+          key: "brand",
+          sortable: true,
+        },
+        {
+          key: "age",
+          label: "Age",
+          sortable: true,
+        },
+      ],
+      filter: null,
+      filterOn: [],
+    };
+  },
+  methods: {
+    UpdateValues(e) {
+      this.barMinValue = e.minValue;
+      this.barMaxValue = e.maxValue;
+    },
+    sortChanged(key) {
+      fetch(`https://localhost:5173/cars/index?sort=${key}`, {
+        method: "GET",
+        mode: "no-cors",
+        cache: 'no-cache',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      }).catch(result => {
+        alert(`Запрос отправлен с параметром ${key.sortBy}`);
+      })
+    },
+  },
+  computed: {
+    filteredData() {
+      return this.parsedData.filter((item) => {
+        if (
+          item["age"] >= this.barMinValue &&
+          item["age"] <= this.barMaxValue
+        ) {
+          return true;
+        } else {
+          return false;
+        }
+      });
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 h3 {
   margin: 40px 0 0;
@@ -54,4 +128,13 @@ li {
 a {
   color: #42b983;
 }
+.multi-range-slider {
+  max-width: 600px;
+  margin: 0 auto;
+}
+.container{
+  max-width: 700px;
+  margin: 0 auto;
+}
+
 </style>
